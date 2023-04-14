5b5978381a912376b8082971b11c8d2a55e84d41
Matt Busigin <matt@confabulation.ai>
2023-04-14 19:41:09 -0400

    First step at solving patching problem

--- src/runner.ts
+++ src/runner.ts
@@ -3,6 +3,7 @@ import * as child_process from 'child_process';
 import * as yaml from 'js-yaml';
 import * as path from 'path';
 import * as os from 'os';
+import * as diff from 'diff';
 
 export const executeCommands =
  async (commands: string[]) => {
@@ -18,7 +19,43 @@ export const executeCommands =
   }
 };
 
+async function applyPatch(targetFile: string, operation: string, content: string) {
+  try {
+    const fileContent = fs.readFileSync(targetFile, 'utf-8');
+    const patches = diff.parsePatch(content) as diff.ParsedDiff[];
 
+    if (operation === 'add') {
+      const patchedContent = patches.reduce((content, patch) => diff.applyPatch(content, patch), fileContent);
+      fs.writeFileSync(targetFile, patchedContent);
+    } else if (operation === 'delete') {
+      const reversedPatches = patches.map(patch => {
+        const { hunks } = patch;
+        const reversedHunks = hunks.map(hunk => {
+          const { oldStart, oldLines, newStart, newLines, lines } = hunk;
+          return {
+            oldStart: newStart,
+            oldLines: newLines,
+            newStart: oldStart,
+            newLines: oldLines,
+            lines: lines.map(line => {
+              if (line.startsWith('-')) return '+' + line.slice(1);
+              if (line.startsWith('+')) return '-' + line.slice(1);
+              return line;
+            }),
+          };
+        });
+        return { ...patch, hunks: reversedHunks };
+      });
+
+      const patchedContent = reversedPatches.reduce((content, patch) => diff.applyPatch(content, patch), fileContent);
+      fs.writeFileSync(targetFile, patchedContent);
+    } else {
+      console.error(`Invalid patch operation: ${operation}`);
+    }
+  } catch (error: unknown) {
+    console.error(`Error applying patch: ${(error as Error).message}`);
+  }
+}
 
 export const processYamlObjects =
  (yamlObjects: any[]) => {
@@ -37,6 +74,9 @@ export const processYamlObjects =
           fs.writeFileSync(obj.filename, obj.contents);
         }
         break;
+      case 'patch':
+        applyPatch(obj.targetFile, obj.operation, obj.content);
+        break;
       default:
         console.error(`Invalid YAML object type: ${obj.type}`);
         break;
@@ -47,7 +87,6 @@ export const processYamlObjects =
 const yamlFilePath = process.argv[2];
 
 if (!yamlFilePath) {
-
   console.error('Please provide a YAML file path as an argument.');
   process.exit(1);
 }
@@ -55,7 +94,4 @@ if (!yamlFilePath) {
 const yamlContent = fs.readFileSync(yamlFilePath, 'utf-8');
 const yamlObjects = yaml.load(yamlContent);
 
-processYamlObjects(<any> yamlObjects); // TODO: we should have better type safety here....
-// This function executes an array of commands.\n
-// This function processes YAML objects and performs the corresponding actions.\n
-// The main function that reads the YAML file and processes the YAML objects.\n
+processYamlObjects(<any> yamlObjects);
